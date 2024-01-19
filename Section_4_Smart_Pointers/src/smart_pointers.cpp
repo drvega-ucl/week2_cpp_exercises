3886f2c762cf6e0b036e445f02a4594e3fa2cca1
Daniel Vega <ucapdrv@ucl.ac.uk>
2024-01-19 16:42:03 +0000

    Added example between raw pointer and smart pointer

--- Section_4_Smart_Pointers/src/smart_pointers.cpp
+++ Section_4_Smart_Pointers/src/smart_pointers.cpp
@@ -25,6 +25,14 @@ class Reporter
 
 int main()
 {
-    
+
+    //This is a raw pointer, it only points in memory where the data is saved
+    Reporter *Report = new Reporter("abc");
+    //Without the delete function there will be a memory leak
+    delete Report;
+
+    //Making a smart pointer makes it so that it is created and destroyed preventing a memory leak
+    std::unique_ptr<Reporter> Report2 = std::make_unique<Reporter>("efg");
+
     return 0;
 }
