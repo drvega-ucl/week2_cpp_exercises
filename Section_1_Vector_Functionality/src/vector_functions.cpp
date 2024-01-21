6561c6987ce35660cfb2d2b3efedac4e060453a0
Daniel Vega <ucapdrv@ucl.ac.uk>
2024-01-21 18:49:14 +0000

    Added lambda function, and made use of std::count_if

--- Section_1_Vector_Functionality/src/vector_functions.cpp
+++ Section_1_Vector_Functionality/src/vector_functions.cpp
@@ -8,16 +8,18 @@
 
 int countMultiplesOfFive(std::vector<int> inputVector)
 {
-    int count = 0; 
+    //Defining my lambda function
 
-    for(auto it = inputVector.begin(); it != inputVector.end(); it++)
-    {
-        if( *it % 5 == 0 )
-        {
-            count++;
-        }
-    }
-    
+    // the [] initializes my lambda function, in this instance it does not take any variables from around it's scope
+    // () in here I define what sort of parameters my lambda function is taking in. For my case I am taking an interger
+    // -> points to the type the return should be, in this case it should return true if my number is divisble by 5
+    auto isDivBy5 = [](int number) -> bool {
+        return number % 5 == 0;
+    };
+
+    //Use std::count_if function, this function takes a start, end, and lambda function
+    //This function creates a integer variable called in whose value is the number of integers within a vector is divisble by 5
+    int count = std::count_if(inputVector.begin(), inputVector.end(), isDivBy5);
 
     return count;
 }
