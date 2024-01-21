861fb557a1bb543e2bf2c850a89ebd7d67ff6986
Daniel Vega <ucapdrv@ucl.ac.uk>
2024-01-21 21:51:08 +0000

    Declared random number generator and real number distribution in order to properly generate random numbers for x,y,z to do Monte Carlo integration

--- Section_2_Monte_Carlo_Integrator/src/MonteCarlo.cpp
+++ Section_2_Monte_Carlo_Integrator/src/MonteCarlo.cpp
@@ -16,13 +16,18 @@ double IntegrateMonteCarlo3D(int n_points, double min, double max)
     double VolCube = std::pow((max - min), 3);
 
     //set up random number generator here
+    std::mt19937 rng_mt;
+
+    //set up real number distribution
+    std::uniform_real_distribution<double> uniform_dist(min,max);
 
     for(int i = 0; i < n_points; i++)
     {
         //generate random points here
-        double x = 
-        double y =
-        double z = 
+        double x = uniform_dist(rng_mt);
+        double y = uniform_dist(rng_mt);
+        double z = uniform_dist(rng_mt);
+
         if(InsideUnitSphere(x, y, z)) count++;
     }
 
@@ -37,4 +42,6 @@ int main()
     double UnitSphereVol = IntegrateMonteCarlo3D(N_points, -1.0, 1.0);
 
     std::cout << "Volume estimate of sphere using " << N_points << " points = " << UnitSphereVol << std::endl;
+
+    return 0;
 }
